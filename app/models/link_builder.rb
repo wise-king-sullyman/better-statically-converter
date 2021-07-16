58578e68aa6b602bed5900dc764cc410d1be3f5e
Sully <a.m.sullivan.github@gmail.com>
2021-07-15 22:02:14 -0400

    Refactor LinkBuilder to build the link from scratch
    This makes adding the needed content easier and makes it usable on other
    repos as the repo & owner are no longer hard coded.

--- app/models/link_builder.rb
+++ app/models/link_builder.rb
@@ -1,4 +1,12 @@
 class LinkBuilder < ApplicationRecord
   validates :commit, presence: true
   validates :file, presence: true
+
+  def build_link(commit, file)
+    split_commit = commit.split('/')
+    owner = split_commit[3]
+    repo = split_commit[4]
+    hash = split_commit.last
+    "https://cdn.statically.io/gh/#{owner}/#{repo}/#{hash}/#{file}"
+  end
 end
